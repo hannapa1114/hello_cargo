73cf09a34964bcdb64c4903ab006c0ad45e50625
hannapa1114 <jmsong@cvkorea.co.kr>
2023-03-21 18:12:44 +0900

    first commit in rust

---
+++ target/debug/deps/helloCargo-85ef104953729ed4.d
@@ -0,0 +1,5 @@
+/Users/zii/Desktop/sideproject/rustprojects/hello_cargo/target/debug/deps/helloCargo-85ef104953729ed4: src/main.rs
+
+/Users/zii/Desktop/sideproject/rustprojects/hello_cargo/target/debug/deps/helloCargo-85ef104953729ed4.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/helloCargo.d
@@ -0,0 +1,1 @@
+/Users/zii/Desktop/sideproject/rustprojects/hello_cargo/target/debug/helloCargo: /Users/zii/Desktop/sideproject/rustprojects/hello_cargo/src/main.rs
